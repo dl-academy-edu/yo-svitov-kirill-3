d7f73d8e90bb2ca96ef3bc5b68c739f048be140d
kirillsvi <kirillsvitov405@gmail.com>
2023-02-19 21:35:06 +0400

    someUpdates

---
+++ source/js/script.js
@@ -0,0 +1,23 @@
+const scrollBtn = document.querySelector('.scrollTopBtn');
+
+if (!scrollBtn) return;
+
+scrollBtn.addEventListener('click', goScroll);
+window.addEventListener('scroll', trackScroll);
+
+function trackScroll() {
+    const offSet = window.pageYOffset;
+    if (offSet > 1500) {
+        scrollBtn.classList.add('scrollTopBtn-show')
+    } else {
+        scrollBtn.classList.remove('scrollTopBtn-show')
+    }
+}
+
+
+function goScroll() {
+    if (window.pageYOffset > 0) {
+        window.scrollBy(0, -50);
+        setTimeout(goScroll, 0);
+    }
+}
